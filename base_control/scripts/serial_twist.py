f4d608d90019fd5cd444d56690494577e9b82b0a
tomokawa <tomokawa.takumi163@mail.kyutech.jp>
2021-11-15 01:13:27 +0900

    fix serial connection

--- base_control/scripts/serial_twist.py
+++ base_control/scripts/serial_twist.py
@@ -10,20 +10,21 @@ class SerialTwist():
     def __init__(self):
         rospy.init_node("serialtwist", anonymous=True)
         rospy.Subscriber("/base_velocity_controller/cmd_vel", Twist, self.callback,queue_size=1)
-        self.ser = serial.Serial('/dev/ttyUSB0', 115200)
+        self.ser = serial.Serial('/dev/ttyMotor', 115200)
         
 
     def callback(self, message):
         rospy.loginfo("subscribe cmd_vel")
-        v_x = str(message.linear.x)
+        v_x = str(message.linear.x/0.95)
         v_y = str(message.linear.y)
         omega = str(message.angular.z)
         print(v_x, v_y, omega)
         self.ser.write(str.encode(v_x+','+v_y+','+omega+'\n\n'))
-
+    
 
 if __name__ == "__main__":
     try:
+        print("Start serial connection")
         serialtwist = SerialTwist()
         rospy.spin()
 
