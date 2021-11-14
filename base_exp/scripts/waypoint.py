e1d9f766312e1fa344cd66b88dd6f9d0502b0e4e
tomokawa <tomokawa.takumi163@mail.kyutech.jp>
2021-11-15 01:16:25 +0900

    fix waypoint script

--- base_exp/scripts/waypoint.py
+++ base_exp/scripts/waypoint.py
@@ -5,16 +5,20 @@ import rospy
 import tf
 import actionlib
 from actionlib_msgs.msg import *
+from std_msgs.msg import String
 from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point, Quaternion, Twist
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from math import pi
 
 class WpNavi():
     def __init__(self):
-        self.way_point = [[0.2, 0.0,0.0 * pi], [999, 999, 999]]
+        self.way_point = [[1.1, 0.25, 0.0], [1.6, 0.25, 0.0],[2.1, 0.25, 0.0], [1.5, 0.20, 0.0],[1.0, 0.20, 0.0],[0.0, 0.00, 0.0]]
+        #self.goal_pos = [[0.5,0.0,0.0],[1.0,0.0,0.0],[1.5,0.0,0.0]]
         self.ac = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.goal = MoveBaseGoal()
         rospy.on_shutdown(self.shutdown)
+
+        self.dolly_xarm_pub = rospy.Publisher("dolly_xarm",String,queue_size=1)
 
     def shutdown(self):
         rospy.loginfo("The robot was terminated.")                               
@@ -26,28 +30,39 @@ class WpNavi():
         rospy.loginfo("The server comes up.")
 
         i = 0
+        numPoint = len(self.way_point)
+
+        command = input("Press keywords")
+        if command =='q':
+            exit()
+
         while not rospy.is_shutdown():
+
+            if i > numPoint:
+                break
             self.goal.target_pose.header.frame_id = 't265_odom_frame'
             self.goal.target_pose.header.stamp = rospy.Time.now()
             self.goal.target_pose.pose.position.x = self.way_point[i][0]
             self.goal.target_pose.pose.position.y = self.way_point[i][1]
 
-            if self.way_point[i][0] == 999:
-                break
             q = tf.transformations.quaternion_from_euler(0, 0, self.way_point[i][2])
             self.goal.target_pose.pose.orientation = Quaternion(q[0],q[1],q[2],q[3])
             rospy.loginfo("Sending goal: No" + str(i+1))
             self.ac.send_goal(self.goal)
 
-            succeeded = self.ac.wait_for_result(rospy.Duration(30))
+            succeeded = self.ac.wait_for_result(rospy.Duration(1))
             state = self.ac.get_state()
             if succeeded:
                 rospy.loginfo("Succeeded: No."+str(i+1)+"("+str(state)+")")
             else:
                 rospy.loginfo("Failed: No."+str(i+1)+"("+str(state)+")")
-
+            self.dolly_xarm_pub.publish("start_pick")
             i = i + 1
             rospy.sleep(2)
+
+            #start_move_msg = rospy.wait_for_message("xarm_dolly",String)
+            command = input("Press to continue")
+        print("Done")
 
 if __name__ == '__main__':
     try:
