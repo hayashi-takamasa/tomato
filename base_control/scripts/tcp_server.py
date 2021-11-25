2bdb16a4bbf1a1d7900054f669b28538f7246800
tomokawa <tomokawa.takumi163@mail.kyutech.jp>
2021-11-25 11:45:02 +0900

    update tcp_server

--- base_control/scripts/tcp_server.py
+++ base_control/scripts/tcp_server.py
@@ -15,20 +15,23 @@ from math import pi
 
 class TCP_Server():
     def __init__(self):
-        server_ip = "127.0.0.1"
+        server_ip = "192.168.1.209"
         server_port = 8080
         listen_num = 5
         self.buffer_size = 1024
         self.odom = 0.0
-        self.destination = [1.8, 0.0, 0.0]
+        self.home_position = [0.0, 0.0, 0.0]
+        self.destination = [0.5, 0.0, 0.0]
         self.stop_msg = Twist()
         
-        print("TCP_server start")
+        #TCP Server Initialization
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((server_ip, server_port))
         self.server.listen(listen_num)
-        print("Movebase Action Client start")
+        print("TCP_server start")
+
+        #Movebase Action Client initialization
         self.ac = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.goal = MoveBaseGoal()
         rospy.on_shutdown(self.shutdown)
@@ -38,9 +41,11 @@ class TCP_Server():
         self.ac.cancel_goal()
 
     def odomCallback(self, msg):
-        self.odom = msg.pose.position.x
+        self.odom = msg.pose.pose.position.x
+        # print(self.odom)
 
     def send_stop(self):
+        self.stop_msg.linear.x = 0
         self.stop_msg.linear.y = 0
         self.stop_msg.linear.z = 0
         self.stop_msg.angular.x = 0
@@ -51,13 +56,17 @@ class TCP_Server():
     def send_goal(self):
         self.goal.target_pose.header.frame_id = 't265_odom_frame'
         self.goal.target_pose.header.stamp = rospy.Time.now()
-        self.goal.target_pose.pose.position.x = self.destination[0]
-        self.goal.target_pose.pose.position.y = self.destination[1]
-        q = tf.transformations.quaternion_from_euler(0, 0, self.destination[2])
-        self.goal.target_pose.pose.orientation = Quaternion(q[0],q[1],q[2],q[3])
-
-        # rospy.loginfo("Current Location : " + self.odom.pose.position.x)
-        # rospy.loginfo("Sending goal : " + destination)
+        if(self.odom < self.destination[0]):
+            self.goal.target_pose.pose.position.x = self.destination[0]
+            self.goal.target_pose.pose.position.y = self.destination[1]
+            q = tf.transformations.quaternion_from_euler(0, 0, self.destination[2])
+            self.goal.target_pose.pose.orientation = Quaternion(q[0],q[1],q[2],q[3])
+        if(self.destination[0] <= self.odom):
+            self.goal.target_pose.pose.position.x = self.home_position[0]
+            self.goal.target_pose.pose.position.y = self.home_position[1]
+            q = tf.transformations.quaternion_from_euler(0, 0, self.home_position[2])
+            self.goal.target_pose.pose.orientation = Quaternion(q[0],q[1],q[2],q[3])
+        
         self.ac.send_goal(self.goal)
         succeeded = self.ac.wait_for_result(rospy.Duration(1))
         state = self.ac.get_state()
@@ -72,23 +81,25 @@ class TCP_Server():
     def process(self):
         rospy.Subscriber("/t265/odom/sample", Odometry, self.odomCallback)
         self.stop_vel_pub = rospy.Publisher("/base_velocity_controller/cmd_vel", Twist, queue_size=1)
-
+        client,address = self.server.accept()
         while not rospy.is_shutdown():
-            client,address = self.server.accept()
+            
             print("------------------------------------")
             print("[*] Connected!! [ Source : {}]".format(address))
             data = client.recv(self. buffer_size)
             print("[*] Received Data : {}".format(data))
-            if(data == b"Go"):
+
+            if(data == b"Move"):
                 print("Sending Goal")
                 self.send_goal()
+                client.sendall(b"StartPick")
             if(data == b"Stop"):
                 print("Cancel destination and Sending Stop")
                 self.ac.cancel_goal()
                 self.send_stop()
 
             client.send(b"Received by dolly")
-            client.close()
+        client.close()
 
 
 if __name__ == '__main__':
